126d8136168b868c6b031e52da9826f511eaf36d
AkmalKurbanov <250296a@gmail.com>
2023-03-02 15:34:32 +0600

    projects popup

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -14,6 +14,7 @@ module.exports = function script() {
   return gulp.src([
       'node_modules/jquery/dist/jquery.js',
       'node_modules/swiper/swiper-bundle.min.js',
+      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
       'src/js/scripts/*.js'
     ])
     // подключение библиотек
